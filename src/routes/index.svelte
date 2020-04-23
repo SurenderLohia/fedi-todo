e41aa77f1190eba0ff4142b0b6ec7fa55e6e2a61
Surender Lohia <surender.lohia.e@gmail.com>
2020-04-23 21:33:56 +0530

    Add toggle logic

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,9 +6,17 @@
 
 	let todoList = [];
 	let categories = [];
+	let completedItems = [];
 
 	function getItemsByCategory(list, category) {
 		return list.filter(item => item.category === category.id)
+	}
+
+	function toggleTodoItem(event, id) {
+		const target = event.target;
+		const isChecked = event.target.checked;
+		
+		// call indexeddb update api
 	}
 
 	onMount(async () => {
@@ -27,10 +35,12 @@
 		<div class="wrap">
 			<h1 class="h1">Fedi-Todo</h1>
 			<h3 class="h3 secondary-text-color">Front-end Developer Interview Todo List</h3>
+			<h5>Completed items {todoList.filter(item => item.done).length}</h5>
 		</div>
 	</header>
 	{#each categories as category }
 		<div class="todo" key={category.id}>
+			<!-- Todo Header -->
 			<div class="todo-header">
 				<div class="wrap">
 					<label class="checkbox-label">
@@ -40,11 +50,12 @@
 					<label for={category.id} class="todo-title">{category.name}</label>
 				</div>
 			</div>
+			<!-- Todo List -->
 			<ul class="todo-list">
 			{#each getItemsByCategory(todoList, category) as todoItem}
 				<li class="todo-item">
 					<label class="checkbox-label">
-						<input id={todoItem.id} type="checkbox">
+						<input id={todoItem.id} type="checkbox" on:change={(e) => toggleTodoItem(e, todoItem.id)} bind:checked={todoItem.done}>
 						<span class="checkbox-custom rectangular"></span>
 					</label>
 					<label for={todoItem.id} class="todo-item-text">
