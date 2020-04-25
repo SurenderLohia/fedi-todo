b06af16c71d24ff91e112bf89d9f2a2a384e1006
Surender Lohia <surender.lohia.e@gmail.com>
2020-04-25 09:13:54 +0530

    Deployment fix

--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -2,14 +2,6 @@ var ghpages = require('gh-pages');
 
 ghpages.publish(
     '__sapper__/export/fedi-todo',
-    {
-        branch: 'gh-pages',
-        repo: 'https://github.com/surenderlohia/fedi-todo.git', 
-        user: {
-          name: 'Surender Lohia',
-          email: 'surender.lohia.e@gmail.com'
-        }
-    },
     () => {
         console.log('Deploy Complete!')
     }
--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
-		'fedi-todo',
+		'/fedi-todo',
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
