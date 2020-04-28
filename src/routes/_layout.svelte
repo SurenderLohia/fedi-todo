c45f4e940499f0518d5751f4e96d81b8c1c8cca1
Surender Lohia <surender.lohia.e@gmail.com>
2020-04-28 17:01:21 +0530

    Added Google analytics

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,3 +1,12 @@
+<GoogleAnalytics {stores} id={ga_measurment_id}/>
+
+<script>
+    import GoogleAnalytics from "sapper-google-analytics/GoogleAnalytics.svelte"
+    import { stores } from "@sapper/app"
+
+    let ga_measurment_id = "UA-164813758-1"  // your analytics id
+</script>
+
 <main>
 	<slot></slot>
 </main>
